ffbfeddcd7bb290ff7a9725059e5807d0116d6c2
Eunice <63760394+18283@users.noreply.github.com>
2024-08-31 22:02:21 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game g = new Game(800, 600, "Game");
+            g.Run(60.0);
+
+            //Pruebas p = new Pruebas(800, 600, "pruebas");
+            //p.Run(60.0);
+
+        }
+    }
+}
